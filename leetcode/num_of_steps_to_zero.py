f7778caaac45901b7a041a02e627991841db872c
Evan H L <ehutch7@gmail.com>
2020-09-27 14:10:02 -0400

    Solution for EASY.

---
+++ leetcode/num_of_steps_to_zero.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberOfSteps(self, num: int, steps: int = 0) -> int:
+        if num == 0:
+            return steps
+        if num % 2 == 0:
+            return self.numberOfSteps(int(num/2), steps + 1)
+        else:
+            return self.numberOfSteps(num - 1 , steps + 1)
+
