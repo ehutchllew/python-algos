8ac1c9e144f384e1f0b93169d23d7976b77c99e9
Evan Hutchinson <evan.hutchinson@apiture.com>
2020-10-08 09:58:23 -0400

    Solving array_diff

---
+++ codewars/array_diff.py
@@ -0,0 +1,14 @@
+# def array_diff(a, b):
+#     diff_list = []
+#     for i in a:
+#         try:
+#             if b.index(i):
+#                 pass
+#         except ValueError:
+#             diff_list.append(i)
+#     print(diff_list)
+
+
+def array_diff(a, b):
+    diff_list = [i for i in a if i not in b]
+    return diff_list
